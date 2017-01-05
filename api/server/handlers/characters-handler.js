fb82c76f473bdf9a74b0e0d7721c3492ed7b3a09
Clint Goodman <ClintGoodman27@gmail.com>
2017-01-04 21:09:23 -0700

    adding in joi, input validation and output schema validation

--- api/server/handlers/characters-handler.js
+++ api/server/handlers/characters-handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const swapi = require('../data/swapi'),
+const joi = require('joi'),
+	swapi = require('../data/swapi'),
 	transform = require('../transforms/characters-transform');
 
 // Schema: characters
@@ -24,7 +25,57 @@ const getCharacterByName = (request, reply) => {
 		});
 };
 
+const schema = {
+	character: joi.object().keys({
+		id: joi
+			.number()
+			.required()
+			.integer()
+			.min(0)
+			.description('id of the character')
+			.example(1),
+		name: joi
+			.string()
+			.required()
+			.description('name of the character')
+			.example('Luke'),
+		birth: joi
+			.string()
+			.required()
+			.description('birth year of the character')
+			.example('19BBY'),
+		gender: joi
+			.string()
+			.required()
+			.valid('male','female','n/a')
+			.description('gender of the character')
+			.example('male'),
+		home: joi
+			.string()
+			.required()
+			.description('home of the character')
+			.example('Tatooine'),
+		films: joi
+			.array()
+			.items(
+				joi
+				.string()
+				.required()
+				.description('name of film')
+				.example('Revenge of the Sith')
+			)
+	}).label('character'),
+	// Using a getter here so that I can refer to `this`, in reference to the character node of the schema object
+	get characters () {
+		return joi
+			.array()
+			.items(this.character)
+			.label('list_of_characters')
+	}
+};
+
 module.exports = {
 	getAllCharacters,
-	getCharacterByName
+	getCharacterByName,
+	schema
 };
--- api/server/init/routes.js
+++ api/server/init/routes.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const handlers = require('../handlers');
+const joi = require('joi'),
+	handlers = require('../handlers');
 
 /*
 
 ROUTES NOTES:
+
+Validation:
+	- Use the validation to validate the incoming request's query, params, and payload
+
+Response Schema:
+	- Validates the outgoing response, making sure it contains all of the appropriate object keys and types
 
 */
 
@@ -14,14 +21,48 @@ const routes = (server) => [{
 	path: '/api/v1/characters',
 	config: {
 		handler: handlers.characters.getAllCharacters,
-		description: 'Gets all the Star Wars characters'
+		description: 'Gets all the Star Wars characters',
+		validate: {
+			query: {
+				page: joi
+					.number()
+					.integer()
+					.min(0)
+					.default(1)
+					.description('page number of results'),
+				limit: joi
+					.number()
+					.integer()
+					.min(0)
+					.default(10)
+					.description('number of results to show per page'),
+				sort: joi
+					.string()
+					.default("name")
+					.description('the key on which to sort results')
+			}
+		},
+		response: {
+			schema: handlers.characters.schema.characters
+		}
 	}
 },{
 	method: 'GET',
 	path: '/api/v1/character/{name}',
 	config: {
 		handler: handlers.characters.getCharacterByName,
-		description: 'Gets info on a single Star Wars character'
+		description: 'Gets info on a single Star Wars character',
+		validate: {
+			params: {
+				name: joi
+					.string()
+					.required()
+					.description('the name of Star Wars character')
+			}
+		},
+		response: {
+			schema: handlers.characters.schema.character
+		}
 	}
 }];
 
